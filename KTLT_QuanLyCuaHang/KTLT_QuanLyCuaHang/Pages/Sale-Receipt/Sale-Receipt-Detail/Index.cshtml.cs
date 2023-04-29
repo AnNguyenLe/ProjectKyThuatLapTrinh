bfdd0adb4a99d797af0cfc5385e9c1709f02a60c
AnNguyenLe <annguyenlemt@gmail.com>
2023-04-29 10:38:01 +0700

    pages for Sale Receipt Detail and Purchase Invoice Detail

---
+++ KTLT_QuanLyCuaHang/KTLT_QuanLyCuaHang/Pages/Sale-Receipt/Sale-Receipt-Detail/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KTLT_QuanLyCuaHang.Pages.Sale_Receipt.Sale_Receipt_Detail
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
